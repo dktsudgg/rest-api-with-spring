8b7dc991963125073d6752038849d757eb2e19d0
dktsudgg <dktsudgg@yonsei.ac.kr>
2020-12-06 02:48:59 +0900

    test(update): 이벤트 생성 api 테스트코드 작성

--- src/test/java/me/dktsudgg/demostudyrestapi/events/EventControllerTests.java
+++ src/test/java/me/dktsudgg/demostudyrestapi/events/EventControllerTests.java
@@ -3,8 +3,10 @@ package me.dktsudgg.demostudyrestapi.events;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -18,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest
+@WebMvcTest // 레포지토리는 주입을 못받음. 왜냐면 웹용 빈들만 생성하고, 레포지토리를 빈으로 등록해주지 않음.. mocking하면됨
 public class EventControllerTests {
 
     @Autowired
@@ -30,8 +32,12 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEvent() throws Exception {
+        // Given
         Event event = Event.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
@@ -44,7 +50,12 @@ public class EventControllerTests {
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타텁 팩토리")
                 .build();
+        event.setId(10);
 
+        // when
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+        // Then
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaTypes.HAL_JSON)
