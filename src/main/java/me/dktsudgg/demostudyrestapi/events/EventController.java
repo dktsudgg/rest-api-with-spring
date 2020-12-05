80f0b52c443ead566c3399913cfb8aff345a4e1e
limkyoujin <dktsudgg@gmail.com>
2020-12-06 06:18:43 +0900

    test(update): 컨트롤러에 입력값 자동검증로직 추가
    빈 값 검증

--- src/main/java/me/dktsudgg/demostudyrestapi/events/EventController.java
+++ src/main/java/me/dktsudgg/demostudyrestapi/events/EventController.java
@@ -1,6 +1,7 @@
 package me.dktsudgg.demostudyrestapi.events;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.internal.Errors;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -25,7 +27,11 @@ public class EventController {
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {  // @Valid 애노테이션은 데이터 바인딩 될 때 값 검증.. 검증 대상 프로퍼티는 해당 클래스에서 애노테이션으로 명세
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
         // 직접 dto->class로 매핑작업을 작성해도 되지만, ModelMapper 라이브러리를 사용하여 EventDto -> Event Class로 매핑할 수 있음.
         // ModelMapper는 공용으로 쓸 수 있기 때문에 Application에 빈으로 등록해놓고 사용..
         Event event = modelMapper.map(eventDto, Event.class);
